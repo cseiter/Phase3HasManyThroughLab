7f558e3917dd2ecaf316d12c4894409083f8f6c7
chris <cseiter@gmail.com>
2021-07-17 21:09:35 -0400

    class Genre creation

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,27 @@
+class Genre
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name=name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|ind_genre| ind_genre.genre == self}
+    end
+
+    def artists
+        songs.map(&:artists)
+    end
+end
