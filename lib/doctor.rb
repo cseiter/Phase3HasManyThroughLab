ff4877ae7f4a84cd04c160cc898f9cb1112931e4
chris <cseiter@gmail.com>
2021-07-17 21:22:39 -0400

    create Doctor class

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+class Doctor
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name=name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date,patient)
+        Appointment.new(date,patient,self)
+    end
+
+    def appointments
+        Appointment.all.select {|ind_appointment| ind_appointment.doctor == self}
+    end
+
+    def patients
+        appointments.map(&:patient)
+    end
+
+end
