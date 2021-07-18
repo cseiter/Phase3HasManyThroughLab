3b218fcf92ab79531c7b2fa3f1a1da3750d82641
chris <cseiter@gmail.com>
2021-07-17 21:03:54 -0400

    class Artist creation

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,32 @@
+class Artist
+
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name=name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def new_song(name,genre)
+        Song.new(name,self,genre)
+    end
+
+    def songs
+        Song.all.select {|ind_song| ind_song.artist == self}
+    end
+
+    def genres
+        songs.map(&:genre)
+    end
+end
 
